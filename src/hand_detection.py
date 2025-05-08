e690cd42bbe0e2ef408602aae686c8d077e7b391
Taynara Araújo de Assis <112909056+tay-assis@users.noreply.github.com>
2025-05-08 13:06:09 -0300

    Criando função que aplica cor de fundo

--- src/hand_detection.py
+++ src/hand_detection.py
@@ -1,6 +1,9 @@
+from email.mime import image
+from unittest import result
 import mediapipe as mp
 import cv2
-import os
+
+import numpy as np
 from calculation_amplitude import CalculationAmplitudeClass
 from vector_drawer import VectorDrawer
 
@@ -9,6 +12,9 @@ class HandDetection:
     def __init__(self, finger1='THUMB_TIP', finger2='INDEX_FINGER_TIP', min_detection_confidence=0.5, min_tracking_confidence=0.5):
         self.finger1 = finger1
         self.finger2 = finger2
+
+        self.mp_selfie_segmentation = mp.solutions.selfie_segmentation.SelfieSegmentation(model_selection=1)
+
         
         # Detecta apenas uma mão.
         self.mp_drawing = mp.solutions.drawing_utils
@@ -34,6 +40,26 @@ class HandDetection:
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         return image, results
     
+    def background_color(self, image, results=None):
+        # Converte para RGB, que é o formato que o MediaPipe espera
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        # Segmenta a imagem com MediaPipe Selfie Segmentation
+        segmentation_results = self.mp_selfie_segmentation.process(image_rgb)
+
+        # A segmentação gera uma máscara com valores de confiança entre 0 e 1
+        condition = segmentation_results.segmentation_mask > 0.8  # threshold
+
+        # Cria fundo colorido
+        bg_color = (255, 255, 255)  # branco
+        bg_image = np.full(image.shape, bg_color, dtype=np.uint8)
+
+        # Combina imagem original e fundo com base na máscara
+        final_image = np.where(condition[..., None], image, bg_image)
+
+        return final_image
+
+
     # Desenha os pontos das mãos detectados 
     def draw_landmarks(self, image, results):
         if results.multi_hand_landmarks:
@@ -53,7 +79,7 @@ class HandDetection:
                 angle = self.calc_amplitude.calculate_amplitude(vector_1, vector_2)
                 
                 # O resultado do ângulo é exibido
-                cv2.putText(image, f'Ângulo: {angle:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                cv2.putText(image, f'Angulo: {angle:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
     # Executa a detecção da mão através da câmera
     def run(self):
@@ -61,6 +87,11 @@ class HandDetection:
             image, results = self.process_frame()
             if image is None:
                 break
+
+            # Aplica fundo colorido com segmentação corporal
+            # image = self.background_color(image)
+            
+            # Desenha os vetores e ângulo sobre a imagem final
             self.draw_landmarks(image, results)
             cv2.imshow('Hand Detection', image)
             if cv2.waitKey(10) & 0xFF == 27:
