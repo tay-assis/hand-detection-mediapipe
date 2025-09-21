decf9d77efe053177774829cb5a1e02a5caea045
Taynara Araújo de Assis <112909056+tay-assis@users.noreply.github.com>
2025-09-21 11:21:18 -0300

    fix

--- src/vector_drawer.py
+++ src/vector_drawer.py
@@ -2,7 +2,7 @@ import cv2
 
 class VectorDrawer:
     def draw_vector(self, image, coord1, coord2, color=(0, 255, 0), thickness=2):
-        # Draw the connection points on the image
+        # Desenhe os pontos de conexão na imagem
         start_point = (int(coord1.x * image.shape[1]), int(coord1.y * image.shape[0]))
         end_point = (int(coord2.x * image.shape[1]), int(coord2.y * image.shape[0]))
         cv2.line(image, start_point, end_point, color, thickness)
